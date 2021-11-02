b4debf529f6649abc2d0ce254f271804806a5a09
prabhat-cn <prabhat@capitalnumbers.com>
2021-11-02 18:09:05 +0530

    csv generation

--- src/agGrid/Table.js
+++ src/agGrid/Table.js
@@ -50,29 +50,59 @@ const Table = () => {
     sortable: true,
     editable: true,
     filter: true,
+    floatingFilter: true,
+    flex: 1,
+  };
+
+  // for export csv
+  let gridApi;
+
+  const onGridReady = (params) => {
+    gridApi = params.api;
+    console.log('gridApi', gridApi);
+  };
+
+  const onExportClick = () => {
+    gridApi.exportDataAsCsv();
   };
   return (
-    <div
-      className="ag-theme-alpine"
-      style={{ height: 250, width: 600, margin: '0 auto' }}
-    >
-      {/* Type-1 */}
-      {/* <AgGridReact rowData={data}>
+    <>
+      <div>
+        <h2>Static Data Table</h2>
+        <button
+          onClick={() => {
+            onExportClick();
+          }}
+        >
+          Export
+        </button>
+        <div
+          className="ag-theme-alpine"
+          style={{
+            height: 600,
+            width: '100%',
+            // margin: '0 auto'
+          }}
+        >
+          {/* Type- 3 */}
+          <AgGridReact
+            rowData={data}
+            columnDefs={columns}
+            defaultColDef={defaultColDef}
+            onGridReady={onGridReady}
+          />
+          {/* Type-1 */}
+          {/* <AgGridReact rowData={data}>
         <AgGridColumn field="name"></AgGridColumn>
         <AgGridColumn field="age"></AgGridColumn>
         <AgGridColumn field="department"></AgGridColumn>
       </AgGridReact> */}
 
-      {/* Type- 2  for single setup for individual */}
-      {/* <AgGridReact rowData={data} columnDefs={columns} /> */}
-
-      {/* Type- 3 */}
-      <AgGridReact
-        rowData={data}
-        columnDefs={columns}
-        defaultColDef={defaultColDef}
-      />
-    </div>
+          {/* Type- 2  for single setup for individual */}
+          {/* <AgGridReact rowData={data} columnDefs={columns} /> */}
+        </div>
+      </div>
+    </>
   );
 };
 
